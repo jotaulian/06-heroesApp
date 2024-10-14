fab2e2e8ac373970b6b2fcb7abdc64233827213c
jotaulian <julianeggle@hotmail.com>
2024-10-14 17:38:38 +0200

    Fiest commit: basic angular routing skeleton. 2 Layouts (Auth, Heroes) with children pages

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { AddNewPageComponent } from './pages/add-new-page/add-new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+  // localhost:400/heroes
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutPageComponent,
+    children: [
+      {path:'add-new-hero', component: AddNewPageComponent},
+      {path:'search', component: SearchPageComponent},
+      {path:'list', component: ListPageComponent},
+      {path:':id', component: HeroPageComponent},
+      {path:'**', redirectTo:'list'}, // La primera vez 'localhost:400/heroes/' redirige a 'list'
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroesRoutingModule { }
---
+++ src/app/heroes/pages/add-new-page/add-new-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heroes-add-new-page',
+  templateUrl: './add-new-page.component.html',
+  styles: ``
+})
+export class AddNewPageComponent {
+
+}
